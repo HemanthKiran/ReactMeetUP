cbbd6d413e51e273f0975509301279f80d1f4c34
Hemanth kiran <ghemanthkiranreddy2@gmail.com>
2021-12-10 14:13:08 +0530

    initial commit

---
+++ src/pages/Favorites.js
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+function Favorites() {
+    const favoritesCtx = useContext(FavoritesContext);
+    let content;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>youhave no favorites, please Add?</p>
+    } else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+    return (
+        <section>
+            <h1>My favorites</h1>
+            {content}
+        </section>
+    );
+}
+export default Favorites;
